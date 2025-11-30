ccf72ed535e1b030aeb2ca74fd25c906893081f9
eetgeenappels <eetgeenappels@gmail.com>
2025-11-30 20:32:07 +0100

    initial commit

---
+++ common/src/main/java/nl/eetgeenappels/ExampleMod.java
@@ -0,0 +1,9 @@
+package nl.eetgeenappels;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "ssss_veinminer";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
